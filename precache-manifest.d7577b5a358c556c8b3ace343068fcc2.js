2e108b162941c54667d52690f14c7735b8dcb7da
Volodymyr Ivanuts <Ivanyts777@gmail.com>
2020-09-10 14:25:32 +0300

    Updates

---
+++ precache-manifest.d7577b5a358c556c8b3ace343068fcc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1db6cb278725305f17e0433f3f43fae",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "817b276dae32c9a8413a",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.ccddda38.chunk.css"
+  },
+  {
+    "revision": "300fe81e6cac198fca41",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e701108b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e701108b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817b276dae32c9a8413a",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.1fe19855.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
